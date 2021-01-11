141faf9db35387ee42e1f09dd128f5b1e36bb1bc
kosba <mohamed.kosba96@gmail.com>
2021-01-11 22:41:52 +0200

    Edit

---
+++ lib/network/cloud_firestore.dart
@@ -0,0 +1,40 @@
+import 'package:cloth_shop/models/product.dart';
+import 'package:cloth_shop/shared/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirebaseFireStoreService {
+  static FirebaseFirestore fireStore;
+
+  FirebaseFireStoreService() {
+    fireStore = FirebaseFirestore.instance;
+  }
+
+  static Future<DocumentReference> createCollectionAndAddProduct(
+      {ProductModel product}) async {
+    return await fireStore.collection(kProductsCollection).add({
+      kProductName: product.pName,
+      kProductDescription: product.pDescription,
+      kProductLocation: product.pLocation,
+      kProductCategory: product.pCategory,
+      kProductPrice: product.pPrice
+    });
+  }
+
+  static Stream<QuerySnapshot> getProducts() {
+    return fireStore.collection(kProductsCollection).snapshots();
+  }
+
+  static Future<void> deleteProduct({documentId}) async {
+    return await fireStore
+        .collection(kProductsCollection)
+        .doc(documentId)
+        .delete();
+  }
+
+  static Future<void> editProduct({productData, documentId}) async {
+    return await fireStore
+        .collection(kProductsCollection)
+        .doc(documentId)
+        .update(productData);
+  }
+}
