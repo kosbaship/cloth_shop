ba89536dc53702f597f7b835de05397e9a7927bd
Mohamed M. Kosba <mohmed.kosba96gmail.com>
2021-01-23 16:44:50 +0200

    After Stable Channel

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/kosba/Android/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/kosba/AndroidStudioProjects/cloth_shop"
+export "FLUTTER_ROOT=/Users/mohmed.kosba96gmail.com/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mohmed.kosba96gmail.com/AndroidStudioProjects/cloths_shop"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=/home/kosba/Android/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/mohmed.kosba96gmail.com/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
